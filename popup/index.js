286b6292f685229521a8541e011ac5e38a1bc772
Belartale <belartale@gmail.com>
2024-06-17 12:21:05 +0300

    add innerHTML to popup

--- popup/index.js
+++ popup/index.js
@@ -4,5 +4,9 @@ chrome.storage.sync.get(
     /* String or Array */ ["selectedText"],
     function (storage) {
         console.log(`storage.selectedText >>> `, storage["selectedText"]);
+
+        const htmlElementWord = document.querySelector(`#word`);
+
+        htmlElementWord.innerHTML = storage["selectedText"];
     }
 );
