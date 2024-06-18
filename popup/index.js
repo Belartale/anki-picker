2d46ca1eeb2f0f06963565e710ebde1933533bd1
Hawkethrone <b.e.dot@ukr.net>
2024-06-19 00:51:21 +0300

    save

--- popup/index.js
+++ popup/index.js
@@ -1,20 +1,20 @@
 console.log(`start popup/index.js`);
 
-function invoke(action, version, params={}) {
+function invoke(action, version, params = {}) {
     return new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest();
-        xhr.addEventListener('error', () => reject('failed to issue request'));
-        xhr.addEventListener('load', () => {
+        xhr.addEventListener("error", () => reject("failed to issue request"));
+        xhr.addEventListener("load", () => {
             try {
                 const response = JSON.parse(xhr.responseText);
                 if (Object.getOwnPropertyNames(response).length != 2) {
-                    throw 'response has an unexpected number of fields';
+                    throw "response has an unexpected number of fields";
                 }
-                if (!response.hasOwnProperty('error')) {
-                    throw 'response is missing required error field';
+                if (!response.hasOwnProperty("error")) {
+                    throw "response is missing required error field";
                 }
-                if (!response.hasOwnProperty('result')) {
-                    throw 'response is missing required result field';
+                if (!response.hasOwnProperty("result")) {
+                    throw "response is missing required result field";
                 }
                 if (response.error) {
                     throw response.error;
@@ -25,42 +25,43 @@ function invoke(action, version, params={}) {
             }
         });
 
-        xhr.open('POST', 'http://127.0.0.1:8765');
-        xhr.send(JSON.stringify({action, version, params}));
+        xhr.open("POST", "http://127.0.0.1:8765");
+        xhr.send(JSON.stringify({ action, version, params }));
     });
 }
 
-await invoke('createDeck', 6, {deck: 'test1'});
-await invoke('addNote', 6, {
-        "note": {
-            "deckName": "test1",
-            "modelName": "Basic",
-            "fields": {
-                "Front": "front content",
-                "Back": "back content"
+const startCreating = async () => {
+    await invoke("createDeck", 6, { deck: "test1" });
+    /*    await invoke("addNote", 6, {
+        note: {
+            deckName: "test1",
+            modelName: "Basic",
+            fields: {
+                Front: "front content",
+                Back: "back content",
             },
-            "options": {
-                "allowDuplicate": false,
-                "duplicateScope": "deck",
-                "duplicateScopeOptions": {
-                    "deckName": "Default",
-                    "checkChildren": false,
-                    "checkAllModels": false
-                }
+            options: {
+                allowDuplicate: false,
+                duplicateScope: "deck",
+                duplicateScopeOptions: {
+                    deckName: "Default",
+                    checkChildren: false,
+                    checkAllModels: false,
+                },
             },
-        }
-});
-
-const result = await invoke('deckNames', 6);
-console.log(`result createDeck >>> ${result}`);
-
+        },
+    });*/
+    const resultCreateDeck = await invoke("createDeck", 6, { deck: "test1" });
+    console.log("resultCreateDeck >>> ", resultCreateDeck);
+};
+startCreating();
 chrome.storage.sync.get(
     /* String or Array */ ["selectedText"],
     function (storage) {
         console.log(`storage.selectedText >>> `, storage["selectedText"]);
 
-        const htmlElementWord = document.querySelector(`#word`);
+        /*const htmlElementWord = document.querySelector(`#word`);
 
-        htmlElementWord.innerHTML = storage["selectedText"];
+        htmlElementWord.innerHTML = storage["selectedText"];*/
     }
 );
