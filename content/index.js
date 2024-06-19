66008a07b7e71b42c3ebee4573180c1a329d0f46
FornRo <belan.artem2012@gmail.com>
2024-06-19 23:14:24 +0200

    [FIX] added testing version with hardcod.

--- content/index.js
+++ content/index.js
@@ -1,5 +1,60 @@
 console.log(`start content/index.js`);
 
+
+function invoke(action, version, params = {}) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.addEventListener("error", () => reject("failed to issue request"));
+        xhr.addEventListener("load", () => {
+            try {
+                const response = JSON.parse(xhr.responseText);
+                if (Object.getOwnPropertyNames(response).length != 2) {
+                    throw "response has an unexpected number of fields";
+                }
+                if (!response.hasOwnProperty("error")) {
+                    throw "response is missing required error field";
+                }
+                if (!response.hasOwnProperty("result")) {
+                    throw "response is missing required result field";
+                }
+                if (response.error) {
+                    throw response.error;
+                }
+                resolve(response.result);
+            } catch (e) {
+                reject(e);
+            }
+        });
+
+        xhr.open("POST", "http://127.0.0.1:8765");
+        xhr.send(JSON.stringify({ action, version, params }));
+    });
+};
+
+
+const startCreating = async (input_data) => {
+    await invoke("addNote", 6, {
+        note: {
+            deckName: "test1",
+            modelName: "Basic",
+            fields: {
+                Front: input_data,
+                Back: "back content",
+            },
+            options: {
+                allowDuplicate: false,
+                duplicateScope: "deck",
+                duplicateScopeOptions: {
+                    deckName: "Default",
+                    checkChildren: false,
+                    checkAllModels: false,
+                },
+            },
+        },
+    });
+};
+
+
 document.addEventListener("dblclick", (event) => {
     const selectedText = window.getSelection().toString();
 
@@ -14,6 +69,9 @@ document.addEventListener("dblclick", (event) => {
         /* String or Array */ ["selectedText"],
         function (storage) {
             console.log(`storage.selectedText >>> `, storage["selectedText"]);
+            if (storage["selectedText"]) {
+                startCreating(input_data=storage["selectedText"]);
+            };
         }
     );
 });
