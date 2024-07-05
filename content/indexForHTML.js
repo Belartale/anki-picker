cea29e4f6daec8dca0db9aab5ba79c661e5ab01e
Belartale <belartale@gmail.com>
2024-07-05 17:56:32 +0300

    added opportunity to add new card

--- content/indexForHTML.js
+++ content/indexForHTML.js
@@ -28,27 +28,139 @@ function invoke(action, version, params = {}) {
     });
 }
 
-const resultModelNames = await invoke("modelNames", 6);
+class StorageHepper {
+    static set({ key, value, callback }) {
+        if (!key || !value) {
+            throw new Error("Key or value not found");
+        }
 
-const deckHTMLElement = document.querySelector(`#deck`);
-deckHTMLElement.innerHTML = resultModelNames
-    .map((deck) => `<option value="${deck}">${deck}</option>`)
-    .join("");
+        chrome.storage.sync.set({ [key]: value }, callback);
+    }
 
-deckHTMLElement.addEventListener("change", async (event) => {
-    const gotModelName = event.target.value;
-    console.log(
-        "deckHTMLElement.addEventListener => gotModelName:",
-        gotModelName
-    );
+    static get({ key, callback }) {
+        if (!key) {
+            throw new Error("Key not found");
+        } else if (!callback) {
+            throw new Error("Callback not found");
+        }
 
-    const gotFindModelsByName = await invoke("findModelsByName", 6, {
-        modelNames: [gotModelName],
+        chrome.storage.sync.get([key], function (storage) {
+            if (storage[key]) {
+                callback(storage[key]);
+            } else {
+                throw new Error('Value from "chrome.storage" not found');
+            }
+        });
+    }
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const deckHTMLElement = document.querySelector(`#deck`);
+    const modelHTMLElement = document.querySelector(`#model`);
+    const frontHTMLElement = document.querySelector(`#front`);
+    const backHTMLElement = document.querySelector(`#back`);
+    const submitHTMLElement = document.querySelector(`#submit`);
+
+    if (
+        !deckHTMLElement ||
+        !modelHTMLElement ||
+        !frontHTMLElement ||
+        !backHTMLElement ||
+        !submitHTMLElement
+    ) {
+        throw new Error("Elements not found");
+    }
+
+    //! Deck
+    const resultDeckNames = await invoke("deckNames", 6);
+
+    deckHTMLElement.innerHTML = resultDeckNames
+        .map((deck) => `<option value="${deck}">${deck}</option>`)
+        .join("");
+
+    StorageHepper.get({
+        key: "deck",
+        callback: (valueFromStorage) => {
+            deckHTMLElement.value = valueFromStorage;
+        },
     });
-    console.log(
-        "deckHTMLElement.addEventListener => gotFindModelsByName:",
-        gotFindModelsByName
-    );
+
+    deckHTMLElement.addEventListener("change", async (event) => {
+        const inputtedDeckName = event.target.value;
+
+        StorageHepper.set({
+            key: "deck",
+            value: inputtedDeckName,
+        });
+    });
+
+    //! Model
+    const resultModelNames = await invoke("modelNames", 6);
+
+    modelHTMLElement.innerHTML = resultModelNames
+        .map((model) => `<option value="${model}">${model}</option>`)
+        .join("");
+
+    StorageHepper.get({
+        key: "model",
+        callback: (valueFromStorage) => {
+            modelHTMLElement.value = valueFromStorage;
+        },
+    });
+
+    modelHTMLElement.addEventListener("change", async (event) => {
+        const inputtedModelName = event.target.value;
+
+        // chrome.storage.sync.set({ model: inputtedModelName }, () => { });
+        StorageHepper.set({
+            key: "model",
+            value: inputtedModelName,
+        });
+
+        // const gotFindModelsByName = await invoke("findModelsByName", 6, {
+        //     modelNames: [gotModelName],
+        // });
+    });
+
+    submitHTMLElement.addEventListener("click", async () => {
+        StorageHepper.get({
+            key: "deck",
+            callback: (deckFromStorage) => {
+                StorageHepper.get({
+                    key: "model",
+                    callback: async (modelFromStorage) => {
+                        if (!deckFromStorage || !modelFromStorage) {
+                            throw new Error(
+                                '"deckFromStorage" or "modelFromStorage" not found'
+                            );
+                        }
+
+                        const result = await invoke("addNote", 6, {
+                            note: {
+                                deckName: deckFromStorage,
+                                modelName: modelFromStorage,
+                                fields: {
+                                    Front: frontHTMLElement.value,
+                                    Back: backHTMLElement.value,
+                                },
+                                options: {
+                                    allowDuplicate: false,
+                                    duplicateScope: "deck",
+                                    duplicateScopeOptions: {
+                                        deckName: "Default",
+                                        checkChildren: false,
+                                        checkAllModels: false,
+                                    },
+                                },
+                            },
+                        });
+
+                        console.log(`result addNote >>> `, result);
+                    },
+                });
+            },
+        });
+    });
 });
 
 // // Прослушивание сообщений от родительского окна
